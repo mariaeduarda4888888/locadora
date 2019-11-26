cd386176253e8e2971ee8d8f160090891dd7fb8c
mxxxx15 <45474888+mxxxx15@users.noreply.github.com>
2019-11-26 13:31:42 -0200

    Criamos o banco de dados; modificação; Criamos os pacotes;

--- Documents/locadora/src/DAO/CategoriaDAO.java
+++ Documents/locadora/src/DAO/CategoriaDAO.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package principal;
+package DAO;
 
-
-public class main {
-   public static void main(String[]args){
-   new Login().setVisible(true); 
-   }     
+/**
+ *
+ * @author Eduarda
+ */
+public class CategoriaDAO {
+    
 }
-
