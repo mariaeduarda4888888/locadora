dceb61bce3a8931509b6a5ae8525bb7357e28aaa
XD <XD@LAPTOP-I5TODLL9>
2019-12-02 13:48:18 -0300

    getter e setter

--- Documents/locadora/src/Modelo/Funcionario.java
+++ Documents/locadora/src/Modelo/Funcionario.java
@@ -11,4 +11,43 @@ package Modelo;
  */
 public class Funcionario {
     
+    private int cod;
+    private String noome;
+    private String login;
+    private String senha;
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getNoome() {
+        return noome;
+    }
+
+    public void setNoome(String noome) {
+        this.noome = noome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
 }
